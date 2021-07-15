a73656084ba813bbd31fe9659a16d621fdbb86d8
DakotaSpencer <>
2021-07-15 16:15:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const mySecret = process.env['TOKEN']
+const Discord = require("discord.js")
+const fetch = require("node-fetch")
+const client = new Discord.Client()
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.author.bot) return
+
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
+  }
+})
+
+client.on("message", msg => {
+   if (msg.content === "./DevTest_Bot_Online") {
+    msg.channel.send("Bot is Online");
+  }
+})
+
+client.login(process.env.TOKEN)
